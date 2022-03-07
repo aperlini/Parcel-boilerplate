5a2dbf3ec241f6e292e90e227d313da90f520fed
Adriano Perlini <aperlini@protonmail.ch>
2022-03-07 19:53:30 +0100

    test config update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 import './styles/override.css'
 
 console.log("default ../src/app.js");
+
+export function sum(a, b) {
+    return a + b;
+}
---
+++ tests/app.spec.js
@@ -0,0 +1,16 @@
+import { sum } from '../src/app'
+
+describe("default tests", () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(sum(1, 2)).toBe(3)
+    });
+
+    test('null', () => {
+        const n = null;
+        expect(n).toBeNull();
+        expect(n).toBeDefined();
+        expect(n).not.toBeUndefined();
+        expect(n).not.toBeTruthy();
+        expect(n).toBeFalsy();
+    });
+})
